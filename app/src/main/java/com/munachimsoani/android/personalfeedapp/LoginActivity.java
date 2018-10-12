82a8ebda07b1287eb7b34cd61046c6301a25a6b3
munachiani <mailstomuna@gmail.com>
2018-10-11 23:08:03 -0700

    Implemented sign in with google using firebase

--- app/src/main/java/com/munachimsoani/android/personalfeedapp/LoginActivity.java
+++ app/src/main/java/com/munachimsoani/android/personalfeedapp/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.button.MaterialButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -16,10 +17,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import static com.munachimsoani.android.personalfeedapp.R.menu.register_menu;
 
@@ -28,6 +38,10 @@ public class LoginActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private TextInputLayout mEmailTextInputLayout;
     private TextInputLayout mPasswordTextInputLayout;
+    private SignInButton signInButton;
+    private  GoogleSignInClient mGoogleSignInClient;
+    private int RC_SIGN_IN = 1;
+    private  final String TAG = "Personal Feed App";
 
     // Firebase Instance Variable
 
@@ -46,12 +60,98 @@ public class LoginActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Sign In");
 
+        // Set the dimensions of the sign-in button.
+         signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
 
         // Get hold of the firebase instance variable
         mAuth = FirebaseAuth.getInstance();
 
+        // Click the google sing button to sign in
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
 
 
+
+    }
+
+
+    // Sign into with google
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI();
+
+//                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                            finish();
+//                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            //Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            showErrorDialog("Authentication Failed");
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void updateUI() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        finish();
+        startActivity(intent);
     }
 
     // Executed when Sign Up button is pressed.
@@ -133,6 +233,8 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
+
+    //
     public void attemptRegistration(){
         if(!validateEmail() | !validatePassword()){
             return;
@@ -152,9 +254,10 @@ public class LoginActivity extends AppCompatActivity {
                     if(!task.isSuccessful()){
                         showErrorDialog("There was a problem signing in!. Please check your login details");
                     } else {
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        finish();
-                        startActivity(intent);
+//                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                        finish();
+//                        startActivity(intent);
+                        updateUI();
                     }
                 }
             });
