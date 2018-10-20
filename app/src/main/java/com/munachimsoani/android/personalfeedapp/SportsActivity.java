e41df2908c02e4a754160851bc51e32f34bceb2e
munachiani <mailstomuna@gmail.com>
2018-10-20 15:37:54 -0700

    implemented Top Stories and Sports using Loaders

--- app/src/main/java/com/munachimsoani/android/personalfeedapp/SportsActivity.java
+++ app/src/main/java/com/munachimsoani/android/personalfeedapp/SportsActivity.java
@@ -2,9 +2,14 @@ package com.munachimsoani.android.personalfeedapp;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -31,13 +36,25 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class SportsActivity extends AppCompatActivity {
+public class SportsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+
+    /* A constant to save and restore the URL that is being displayed */
+    private static final String NEWS_QUERY_URL_EXTRA = "query";
+
+    /*
+     * This number will uniquely identify our Loader and is chosen arbitrarily. You can change this
+     * to any number you like, as long as you use the same variable name.
+     */
+    private static final int NEWS_LOADER = 22;
+
+    private final String TAG = "MainActivity";
+
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mActionBarDrawerToggle;
     private Toolbar mToolbar;
 
-    private TextView mDispalyName;
+    private TextView mDisplayName;
     private TextView mUserEmail;
 
     private RecyclerView mRecyclerView;
@@ -46,10 +63,11 @@ public class SportsActivity extends AppCompatActivity {
 
     private ProgressBar mProgressBar;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sports);
+        setContentView(R.layout.activity_main);
 
         mProgressBar = findViewById(R.id.progressbar);
 
@@ -75,7 +93,7 @@ public class SportsActivity extends AppCompatActivity {
         View navHeaderView = navigationView.getHeaderView(0);
 
 
-        mDispalyName = navHeaderView.findViewById(R.id.username);
+        mDisplayName = navHeaderView.findViewById(R.id.username);
         mUserEmail = navHeaderView.findViewById(R.id.user_email);
 
 
@@ -86,7 +104,7 @@ public class SportsActivity extends AppCompatActivity {
             String name = user.getDisplayName();
             String email = user.getEmail();
 
-            mDispalyName.setText(name);
+            mDisplayName.setText(name);
             mUserEmail.setText(email);
 
         }
@@ -99,8 +117,8 @@ public class SportsActivity extends AppCompatActivity {
 
         mRecyclerView.addItemDecoration(new SimpleDividerItemDecoration(mRecyclerView.getContext(), DividerItemDecoration.VERTICAL));
 
-
-        makeNetworkQuerySports();
+//      call makeNetworkQueryTopStories
+        makeNetworkQueryTopStories();
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -127,14 +145,31 @@ public class SportsActivity extends AppCompatActivity {
             }
         });
 
+
     }
 
-
-    public void makeNetworkQuerySports() {
+    // Create a method to get the news URL
+    public void makeNetworkQueryTopStories() {
         URL networkURL = NetworkUtils.buildUrlSports();
-        Log.d("test", networkURL.toString());
+//        Log.d(TAG, networkURL.toString());
         // mSearchResultsTextView.setText(networkURL.toString());
-        new NewsTask().execute(networkURL);
+
+//        Create a Bundle to hold the  URL because loaders use bundle
+        Bundle queryBundle = new Bundle();
+//        Put the value of the URL inside the bundle
+        queryBundle.putString(NEWS_QUERY_URL_EXTRA, networkURL.toString());
+
+//        Call getSupportLoaderManager and store it in a LoaderManager variable
+        LoaderManager loaderManager = getSupportLoaderManager();
+        Loader<String> networkQueryTopStories = loaderManager.getLoader(NEWS_LOADER);
+//        new NewsTask().execute(networkURL);
+
+//        If the Loader was null, initialize it. Else, restart it.
+        if (networkQueryTopStories == null) {
+            loaderManager.initLoader(NEWS_LOADER, queryBundle, this);
+        } else {
+            loaderManager.restartLoader(NEWS_LOADER, queryBundle, this);
+        }
     }
 
     @Override
@@ -148,36 +183,72 @@ public class SportsActivity extends AppCompatActivity {
     }
 
 
+    //    Override onCreateLoader
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int i, @Nullable final Bundle bundle) {
+        return new AsyncTaskLoader<String>(this) {
 
-    public class NewsTask extends AsyncTask<URL, Void, String> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mProgressBar.setVisibility(View.VISIBLE);
-        }
 
-        @Override
-        protected String doInBackground(URL... params) {
-            URL searchUrl = params[0];
-            String newsResults = null;
-            try {
-                newsResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
-            } catch (IOException e) {
-                e.printStackTrace();
+            //             Override onStartLoading
+            @Override
+            protected void onStartLoading() {
+                super.onStartLoading();
+//                Set the progress bar visible while fetching data( initally loading in background)
+                mProgressBar.setVisibility(View.VISIBLE);
+
+//                Force a load
+                forceLoad();
             }
-            return newsResults;
-        }
+
+//            Override loadInBackground
+
+            @Nullable
+            @Override
+            public String loadInBackground() {
 
 
-        @Override
-        protected void onPostExecute(String newsResult) {
-            super.onPostExecute(newsResult);
-            mProgressBar.setVisibility(View.GONE);
-            news = JSONUtils.makeNewsListTopStories(newsResult);
-            mAdapter.mNews.addAll(news);
-            mAdapter.notifyDataSetChanged();
+//           Parse the URL from the passed in String and fetch the data
+                String searchQueryUrlString = bundle.getString(NEWS_QUERY_URL_EXTRA);
+                String newsResults = null;
+
+                try {
+                    URL searchUrl = new URL(searchQueryUrlString);
+                    newsResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return newsResults;
 
 
-        }
+            }
+        };
     }
+
+//    Override onLoadFinished
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String newsResult) {
+
+//        Remove(hide) the progress bar after the data have been fetched
+        mProgressBar.setVisibility(View.GONE);
+
+//        Get the POJO of the data
+        news = JSONUtils.makeNewsListTopStories(newsResult);
+
+//        Add the data to Recycler Adapter
+        mAdapter.mNews.addAll(news);
+
+//        Notify the Adapter if there is any change
+        mAdapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
+    }
+
+
 }
